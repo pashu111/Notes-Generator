ee95040852aa5ccaf88024e038edf86986ecadb2
Ashutosh Pradhan <ashutoshpradhan8013@gmail.com>
2026-08-19 16:59:20 +0530

    changes index

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const app = express()
 
 app.use(cors({
     origin: "https://notes-generator-5trfkma4w-pashu111s-projects.vercel.app",
+    http: "localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
